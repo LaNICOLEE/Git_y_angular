f8a603379687ff9f6a5c002eb4d65564de6af360
Nicole <gonza.latorre21@gmail.com>
2023-07-06 11:02:33 -0300

    ComponenteNew

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { item } from '../../models/item';
 
 @Component({
   selector: 'app-items',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent {
+  items:item[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        id: 0,
+        title: 'manzana', 
+        price: 10.5,
+        quantity: 5,
+        completed: false
+      },
+      {
+        id: 1,
+        title: 'pan', 
+        price: 3.5,
+        quantity: 8,
+        completed: true
+      }
+    ];
+  }
 
 }
---
+++ src/app/models/item.ts
@@ -0,0 +1,8 @@
+export class item{
+    id: number = 0;
+    title: string = '';
+    price: number = 0;
+    quantity: number = 0;
+    completed: boolean = false;
+
+}
